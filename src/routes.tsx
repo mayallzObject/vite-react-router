7269c1fe9e06432c687ad39cdb64aa43b1c1bc7c
Ivaylo Yankov <mayallzobject@gmail.com>
2024-03-11 15:48:40 +0100

    feat: create lazy routing and nesting for existing pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,21 +1,79 @@
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import UserProfilePage from './pages/UserProfilePage';
+import { RouteObject } from 'react-router-dom';
+import AppLayout from './layouts/AppLayout';
 
-export const publicRoutes = [
+export const publicRoutes: RouteObject[] = [
   {
     path: '/',
-    element: <HomePage />,
+    element: <AppLayout />,
+    children: [
+      {
+        index: true,
+        async lazy() {
+          const { HomePage } = await import(
+            './pages/home/homepage.component'
+          );
+          return { Component: HomePage };
+        },
+      },
+      {
+        path: 'dashboard',
+        async lazy() {
+          const { DashboardPage } = await import(
+            './pages/dashboard/dashboardpage.component'
+          );
+          return { Component: DashboardPage };
+        },
+      },
+    ],
   },
   {
-    path: '/login',
-    element: <LoginPage />,
+    path: 'login',
+    async lazy() {
+      const { AuthLayout } = await import('./layouts/AuthLayout');
+      return { Component: AuthLayout };
+    },
+    children: [
+      {
+        index: true,
+        async lazy() {
+          const { LoginPage } = await import(
+            './pages/login/loginpage.component'
+          );
+          return { Component: LoginPage };
+        },
+      },
+      {
+        path: 'signup',
+        async lazy() {
+          const { SignupPage } = await import(
+            './pages/signup/signuppage.component'
+          );
+          return { Component: SignupPage };
+        },
+      },
+    ],
   },
 ];
 
-export const privateRoutes = [
+export const privateRoutes: RouteObject[] = [
   {
     path: '/profile',
-    element: <UserProfilePage />,
+    async lazy() {
+      const { ProtectedLayout } = await import(
+        './layouts/ProtectedLayout'
+      );
+      return { Component: ProtectedLayout };
+    },
+    children: [
+      {
+        index: true,
+        async lazy() {
+          const { ProfilePage } = await import(
+            './pages/profile/profilepage.components'
+          );
+          return { Component: ProfilePage };
+        },
+      },
+    ],
   },
 ];
